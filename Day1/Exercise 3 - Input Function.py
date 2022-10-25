eac65a93367bb77413ee55f5e676b4f2c574836a
narcisabadea <vasile.narcisa11@gmail.com>
2022-10-25 19:50:37 +0300

    Initial commit

---
+++ Day1/Exercise 3 - Input Function.py
@@ -0,0 +1,21 @@
+# Write a program that prints the number of characters in a user's name.
+
+# Example Input
+# Angela
+# Example Output
+# 6
+
+def get_length(name):
+    print(len(name))
+
+name = input("What is your name?")
+get_length(name)
+
+
+
+
+
+
+
+
+
--- Day1/README.md
+++ Day1/README.md
@@ -3,3 +3,12 @@
 ## Exercise 1 - Printing
 
 ![printing](/Day1/assets//1.1.gif)
+
+## Exercise 2 - Debugging
+
+![printing](/Day1/assets//1.2.gif)
+
+## Exercise 3 - Input
+
+![printing](/Day1/assets//1.3.gif)
+
