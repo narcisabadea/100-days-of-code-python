c7950d51c018f3ae67d5f3e5dc932869755df3e2
narcisabadea <vasile.narcisa11@gmail.com>
2022-11-19 19:13:38 +0200

    Day 4 ex 1

---
+++ Day4/Exercise 1 - Head or tails.py
@@ -0,0 +1,10 @@
+# Write a virtual coin toss program. It will randomly tell the user "Heads" or "Tails".
+# e.g. 1 means Heads 0 means Tails
+
+import random
+
+random_side = random.randint(0, 1)
+if random_side == 1:
+    print("Heads")
+else:
+    print("Tails")
