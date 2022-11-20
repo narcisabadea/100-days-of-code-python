a45898e2295e78288df32bb8c8f134166bd45fea
narcisabadea <vasile.narcisa11@gmail.com>
2022-11-20 20:03:23 +0200

    Started day 4 4x 2

---
+++ Day4/Exercise 2 - Banker Roulette.py
@@ -0,0 +1,7 @@
+# Write a program that will select a random name from a list of names. The person selected will have to pay for everybody's food bill.
+
+# Example Input
+# Angela, Ben, Jenny, Michael, Chloe
+
+# Example Output
+# Michael is going to buy the meal today!
