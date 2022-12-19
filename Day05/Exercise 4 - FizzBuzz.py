85bdf92d590f7806238b0811065e0702b033d771
narcisabadea <vasile.narcisa11@gmail.com>
2022-12-19 09:48:36 +0200

    Day 13

--- Day05/Exercise 4 - FizzBuzz.py
+++ Day05/Exercise 4 - FizzBuzz.py
@@ -4,12 +4,12 @@
 # When the number is divisible by 5, then instead of printing the number it should print "Buzz".`
 # And if the number is divisible by both 3 and 5 e.g. 15 then instead of the number it should print "FizzBuzz"
 
-for n in range(1,101):
-    if n%3 == 0 and n%5 == 0:
+for n in range(1, 101):
+    if n % 3 == 0 and n % 5 == 0:
         print("FizzBuzz")
-    elif n%3 == 0:
+    elif n % 3 == 0:
         print("Fizz")
-    elif n%5 == 0:
+    elif n % 5 == 0:
         print("Buzz")
     else:
         print(n)
